b44b3ad0164f62b1c85e71c73af3e11745b3fd88
Briam <boomyoman@gmail.com>
2023-11-08 16:40:20 -0600

    Finding best wy to go about javascript

---
+++ FrontEnd/app.js
@@ -0,0 +1,15 @@
+// gets ids from index.html so that they can be manipulated
+let titleInput  = document.getElementById("titleInput") 
+
+
+
+// this function will get information using an API
+const getBook = () => { 
+
+    document.getElementById('output').innerHTML = " "
+
+
+
+
+}
+
